0aa2f2135f0ca8d0bdfe4a990c47b70bd6df6e5c
Paulo Souza <32749199+ohenriques@users.noreply.github.com>
2024-07-22 09:46:02 -0300

    Implementando contador

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
-// /**
-//  * Updates the countdown timer based on the provided future date.
-//  * @param {Date} futureDate - The future date to count down to.
-//  */
-// function updateCountdown(futureDate) {
-//     const now = new Date();
-//     const diff = futureDate - now;
+/**
+ * Updates the countdown timer based on the provided future date.
+ * @param {Date} futureDate - The future date to count down to.
+ */
+function updateCountdown(futureDate) {
+    const now = new Date();
+    const diff = futureDate - now;
 
-//     if (diff < 0) {
-//         document.getElementById('time-remaining').innerText = "The countdown has ended!";
-//         return;
-//     }
+    // todo: Tratamento para quando a data já passou
+    if (diff < 0) {
+        document.getElementById('timeRemaining_days').innerText = "The countdown has ended!";
+        return;
+    }
 
-//     const days = Math.floor(diff / 1000 / 60 / 60 / 24);
-//     const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
-//     const minutes = Math.floor(diff / 1000 / 60) % 60;
-//     const seconds = Math.floor(diff / 1000) % 60;
+    const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const minutes = Math.floor(diff / 1000 / 60) % 60;
+    const seconds = Math.floor(diff / 1000) % 60;
 
-//     const months = futureDate.getMonth() - now.getMonth() +
-//         (12 * (futureDate.getFullYear() - now.getFullYear()));
+    const months = futureDate.getMonth() - now.getMonth() +
+        (12 * (futureDate.getFullYear() - now.getFullYear()));
 
-//     document.getElementById('time-remaining').innerText =
-//         `Tempo restante: ${days} dias, ${hours} horas, ${minutes} minutos, ${seconds} segundos`;
-// }
+    document.getElementById('timeRemaining_days').innerText =
+        `${days} `;
+    document.getElementById('timeRemaining_hours').innerText =
+        `${hours} `;
+    document.getElementById('timeRemaining_minutes').innerText =
+        `${minutes} `;
+    document.getElementById('timeRemaining_seconds').innerText =
+        `${seconds} `;
+  
+}
 
-// const futureDate = new Date('2024-09-11T08:45:00');
+const futureDate = new Date('2024-09-11T08:45:00');
 
-// updateCountdown(futureDate);
+updateCountdown(futureDate);
 
-// setInterval(() => {
-//     updateCountdown(futureDate);
-// }, 1000);
+setInterval(() => {
+    updateCountdown(futureDate);
+}, 1000);
 
 window.addEventListener('scroll', function () {
     var nav = document.querySelector('nav'); // Selecione sua navbar
